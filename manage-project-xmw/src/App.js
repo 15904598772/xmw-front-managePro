d481dc335ffccc01dc838fffb0203256ca34f8cc
15904598772 <1620965485@qq.com>
2023-02-08 08:43:08 +0800

    插件安装

--- manage-project-xmw/src/App.js
+++ manage-project-xmw/src/App.js
@@ -1,10 +1,19 @@
+import {BrowserRouter, Route, Routes} from 'react-router-dom'
 import './App.css';
+import Login from './pages/login';
+import Show from './pages/show';
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Routes>
+          <Route path='/' element={<Login></Login>}></Route>
+          <Route path='/show' element={<Show></Show>}></Route>
+        </Routes>
+      </div>
+    </BrowserRouter>
+    
   );
 }
 
